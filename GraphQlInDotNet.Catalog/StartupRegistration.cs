acee23b0fc9954b50f74549a49ca7ef03f6b24e6
schlaffeck <thornis@o2.pl>
2019-10-31 21:22:26 +0100

    startup - registration

---
+++ GraphQlInDotNet.Catalog/StartupRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQlInDotNet.Catalog
+{
+    public static class StartupRegistration
+    {
+        public static IServiceCollection AddCatalogDomain(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<GetAllCategories.GetAllCategoriesQuery>()
+                .AddScoped<CreateCategory.CreateCategoryCommand>();
+        }
+    }
+}
